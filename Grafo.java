5f18c2b7eeeb2ccfb4539fcae39609143d1694c5
rafaeliannini <167246348+rafaeliannini@users.noreply.github.com>
2025-09-20 11:51:09 -0300

    Busca em profundidade
    Realizado uma busca em profundidade iterativa (a recursiva estourava para o arquivo com 1 milhão de arestas)

--- Grafo.java
+++ Grafo.java
@@ -5,6 +5,30 @@ public class Grafo {
     private int numVertices;
     private List<Integer>[] adjacentes;
 
+    private Set<Pair> arestasPai = new HashSet<>();
+    private boolean[] visitado;
+    private int[] descoberta, termino;
+    private int tempo;
+
+    // Classe de pares desenvolvida com ajuda de IA
+    private static class Pair {
+        final int u, v;
+        Pair(int u, int v) { this.u = u; this.v = v; }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Pair)) return false;
+            Pair p = (Pair) o;
+            return u == p.u && v == p.v;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(u, v);
+        }
+    }
+
     public int getNumVertices(){
         return numVertices;
     }
@@ -27,6 +51,10 @@ public class Grafo {
                 int destino = Integer.parseInt(partes[1]);
 
                 adjacentes[origem].add(destino);
+            }
+
+            for (int i = 1; i <= numVertices; i++) {
+                Collections.sort(adjacentes[i]);
             }
         }
     }
@@ -62,4 +90,68 @@ public class Grafo {
         }
         return predecessores;
     }
+
+    public void buscaProfundidade() {
+        visitado = new boolean[numVertices + 1];
+        descoberta = new int[numVertices + 1];
+        termino = new int[numVertices + 1];
+        tempo = 0;
+
+        for (int v = 1; v <= numVertices; v++) {
+            if (!visitado[v]) {
+                buscaProfundidadeIterativa(v);
+            }
+        }
+    }
+
+    // Iterativa pois a recursiva estourava a JVM com o grafo de 1 milhão de arestas
+    private void buscaProfundidadeIterativa(int raiz) {
+        // Pilha e iteradores para simular funcionamento de chamada recursiva, e fazer a busca em ordem lexicográfica
+        Deque<Integer> pilha = new ArrayDeque<>();
+        Deque<Iterator<Integer>> iteradores = new ArrayDeque<>();
+
+        pilha.push(raiz);
+        descoberta[raiz] = ++tempo;
+        visitado[raiz] = true;
+        iteradores.push(adjacentes[raiz].iterator());
+
+        while (!pilha.isEmpty()) {
+            int u = pilha.peek();
+            Iterator<Integer> it = iteradores.peek();
+
+            if (it.hasNext()) {
+                int v = it.next();
+                if (!visitado[v]) {
+                    System.out.println("Aresta de árvore: " + u + " -> " + v);
+                    arestasPai.add(new Pair(u, v));
+
+                    visitado[v] = true;
+                    descoberta[v] = ++tempo;
+
+                    pilha.push(v);
+                    iteradores.push(adjacentes[v].iterator());
+                }
+            } else {
+                termino[u] = ++tempo;
+                pilha.pop();
+                iteradores.pop();
+            }
+        }
+    }
+
+    public void classificarArestasDoVertice(int u) {
+        for (int v : adjacentes[u]) {
+        Pair aresta = new Pair(u, v);
+            if (arestasPai.contains(aresta)) {
+                System.out.println("Aresta (" + u + " -> " + v + "): Árvore");
+            } else if (descoberta[v] < descoberta[u] && termino[v] > termino[u]) {
+                System.out.println("Aresta (" + u + " -> " + v + "): Retorno");
+            } else if (descoberta[u] < descoberta[v] && termino[v] < termino[u]) {
+                System.out.println("Aresta (" + u + " -> " + v + "): Avanço");
+            } else {
+                System.out.println("Aresta (" + u + " -> " + v + "): Cruzamento");
+            }
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println();
             System.out.println("Escolha o arquivo:");
-            System.out.println("1 - graph-test-10-1.txt");
+            System.out.println("1 - graph-test-8-1.txt");
             System.out.println("2 - graph-test-100-1.txt");
             System.out.println("3 - graph-test-50000-1.txt");
             System.out.println("4 - Digitar o nome do arquivo");
@@ -17,7 +17,7 @@ public class Main {
             
             String arquivo;
             switch (opcao) {
-                case 1 -> arquivo = "graph-test-10-1.txt";
+                case 1 -> arquivo = "graph-test-8-1.txt";
                 case 2 -> arquivo = "graph-test-100-1.txt";
                 case 3 -> arquivo = "graph-test-50000-1.txt";
                 case 4 -> {
@@ -45,6 +45,13 @@ public class Main {
                 System.out.println("Grau de entrada de " + vertice + ": " + grafo.grauEntrada(vertice));
                 System.out.println("Sucessores: " + grafo.ListaSucessores(vertice));
                 System.out.println("Predecessores: " + grafo.ListaPredecessores(vertice));
+
+                grafo.buscaProfundidade();
+
+                System.out.println();
+                System.out.println("Classificação das arestas que saem do vértice " + vertice + ":");
+
+                grafo.classificarArestasDoVertice(vertice);
             }
         } catch (IOException e) {
             System.err.println("Erro ao ler arquivo: " + e.getMessage());
